dbc3420d2f7724ac29989425db36357e4aab6443
Tanut-M2 <tanat44@gmail.com>
2022-10-08 13:37:51 +0200

    Initial Commit

---
+++ iPad.SimulatorTest/iPad_SimulatorTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPad_SimulatorTestApp.swift
+//  iPad.SimulatorTest
+//
+//  Created by Tanut on 8/10/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct iPad_SimulatorTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
